3f7a1bc3b747ae72cd4be238c9f3f075815b5807
AZ <aghayret@dcca.com>
2025-01-10 10:14:27 -0500

    Revert "Action Utility class"

--- src/utilities/HelperUtilities.ts
+++ src/utilities/HelperUtilities.ts
@@ -64,8 +64,8 @@ export class HelperUtilities{
      * @param code 
      * @returns 
      */
-    static getStateText(code: string): string{
-        const states: {[key: string]: string} = {
+    static getStateText(code: string): string | null {
+        const states = {
             AL: 'Alabama',
             AK: 'Alaska',
             AZ: 'Arizona',
@@ -77,7 +77,7 @@ export class HelperUtilities{
             FL: 'Florida',
             GA: 'Georgia'
         }
-        return states[code.toUpperCase()] || "Invalid state code";
+        return states[code.toUpperCase()] || null;
     }
 
 
